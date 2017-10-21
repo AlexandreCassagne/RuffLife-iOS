28e163d9c97b3a3193971cf0007b2f6d3df9c0a5
Alexandre Cassagne <alexandre.cassagne@gmail.com>
2017-10-21 16:06:57 -0400

    Added fields in found

--- RuffLife/FoundViewController.swift
+++ RuffLife/FoundViewController.swift
@@ -12,7 +12,9 @@ class FoundViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		
+		
+		
         // Do any additional setup after loading the view.
     }
 
