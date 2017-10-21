4df005b0fad590a9e5f3b4dcc5a76702b062713c
Alexandre Cassagne <alexandre.cassagne@gmail.com>
2017-10-21 15:54:55 -0400

    First commit

--- RuffLife/ViewController.swift
+++ RuffLife/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var lost_found: UISegmentedControl!
+	@IBOutlet weak var dog_cat: UISegmentedControl!
+	@IBAction func submit(_ sender: Any) {
+		let dog = dog_cat.selectedSegmentIndex == 0
+		if (lost_found.selectedSegmentIndex == 1) { // found
+			performSegue(withIdentifier: "foundSegue", sender: self)
+		} else { // lost
+			performSegue(withIdentifier: "lostSegue", sender: self)
+		}
+		print(dog)
+		
+	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
