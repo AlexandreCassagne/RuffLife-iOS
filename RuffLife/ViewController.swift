38811bc2f80c00181d0522a58c00685885ac9c50
Alexandre Cassagne <alexandre.cassagne@gmail.com>
2017-10-21 15:39:21 -0400

    Initial Commit

---
+++ RuffLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RuffLife
+//
+//  Created by Alexandre Cassagne on 21/10/2017.
+//  Copyright © 2017 Cassagne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
