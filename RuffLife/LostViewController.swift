d92ab16646d05a296cfd52ba516a8c7f50c4ec14
Phillip Le <senorkhoa@yahoo.com>
2017-10-21 16:31:45 -0400

    More changes

--- RuffLife/LostViewController.swift
+++ RuffLife/LostViewController.swift
@@ -7,29 +7,57 @@
 //
 
 import UIKit
+import MapKit
 
 class LostViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var mapView: MKMapView!
+    let annotation = MKPointAnnotation()
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self,
+                                   action: #selector(LostViewController.mapTypeChanged(_:)),
+                                   for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 11.12, longitude: 12.11)
+        mapView.addAnnotation(annotation)
+        print("MapViewController loaded its view")
+    }
 
 }
