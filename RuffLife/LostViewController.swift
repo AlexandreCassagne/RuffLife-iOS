4138bf5394f4e08593b4e5cb2891697ab9c16357
Alexandre Cassagne <alexandre.cassagne@gmail.com>
2017-10-21 15:58:01 -0400

    set ib correctly

---
+++ RuffLife/LostViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LostViewController.swift
+//  RuffLife
+//
+//  Created by Alexandre Cassagne on 21/10/2017.
+//  Copyright © 2017 Cassagne. All rights reserved.
+//
+
+import UIKit
+
+class LostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
