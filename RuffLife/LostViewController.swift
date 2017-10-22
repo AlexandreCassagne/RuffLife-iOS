1e566e7ae0c06bbce9f4a8518ba857a0ce19d3ee
Phillip Le <senorkhoa@yahoo.com>
2017-10-21 21:58:31 -0400

    rewriting key

--- RuffLife/LostViewController.swift
+++ RuffLife/LostViewController.swift
@@ -12,7 +12,48 @@ import AWSDynamoDB
 
 class LostViewController: UIViewController {
     var dynamoDBObjectMapper = AWSDynamoDBObjectMapper.default ()
+    var mapView: MKMapView!
+    let annotation = MKPointAnnotation()
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self,
+                                   action: #selector(MapViewController.mapTypeChanged(_:)),
+                                   for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
     
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         dynamoDBObjectMapper.load(RuffLife.self, hashKey: 0, rangeKey:nil).continueWith(block: { (task:AWSTask<AnyObject>!) -> Any? in
@@ -20,7 +61,8 @@ class LostViewController: UIViewController {
                 print("The request failed. Error: \(error)")
             } else if let resultModel = task.result as? RuffLife {
                 // Do something with task.result.
-                print(resultModel.Breed!)
+                print(resultModel.lat!)
+                print(resultModel.lon!)
             }
             return nil
         })
